6057ac988194f89725de14316d589f6529f0b8b7
Cory G Watson <gphat@onemogin.com>
2008-08-02 06:00:33 +0000

    Start on point
    git-svn-id: http://our.coldhardcode.com/svn/Chart-Clicker/branches/g-p@6811 1bc25575-cd0e-40ee-878a-d50c9ce4e587

--- lib/Chart/Clicker/Renderer/Point.pm
+++ lib/Chart/Clicker/Renderer/Point.pm
@@ -17,11 +17,11 @@ has 'shape' => (
     }
 );
 
-sub dontdraw {
-    my $self = shift();
+override('pack', sub {
+    my ($self) = @_;
 
     my $clicker = $self->clicker;
-    my $cr = $clicker->cairo;
+    # my $cr = $clicker->cairo;
 
     my $width = $self->width;
     my $height = $self->height;
@@ -35,27 +35,67 @@ sub dontdraw {
             my $domain = $ctx->domain_axis;
             my $range = $ctx->range_axis;
 
-            my $min = $range->range->lower();
+            my $min = $range->range->lower;
 
-            my $height = $self->height();
+            my $height = $self->height;
 
-            my @vals = @{ $series->values() };
-            my @keys = @{ $series->keys() };
-            for(0..($series->key_count() - 1)) {
+            my @vals = @{ $series->values };
+            my @keys = @{ $series->keys };
+            for(0..($series->key_count - 1)) {
                 my $x = $domain->mark($width, $keys[$_]);
                 my $y = $height - $range->mark($height, $vals[$_]);
 
-                $cr->move_to($x, $y);
-                $self->draw_point($cr, $x, $y, $series, $_);
+                # $cr->move_to($x, $y);
+                # $self->draw_point($cr, $x, $y, $series, $_);
             }
-            my $color = $clicker->color_allocator->next();
-            $cr->set_source_rgba($color->as_array_with_alpha());
-            $cr->fill();
+            my $color = $clicker->color_allocator->next;
+            # $cr->set_source_rgba($color->as_array_with_alpha);
+            # $cr->fill;
         }
     }
 
     return 1;
-}
+});
+
+# sub dontdraw {
+#     my $self = shift;
+# 
+#     my $clicker = $self->clicker;
+#     my $cr = $clicker->cairo;
+# 
+#     my $width = $self->width;
+#     my $height = $self->height;
+# 
+#     my $dses = $clicker->get_datasets_for_context($self->context);
+#     foreach my $ds (@{ $dses }) {
+#         foreach my $series (@{ $ds->series }) {
+# 
+#             # TODO if undef...
+#             my $ctx = $clicker->get_context($ds->context);
+#             my $domain = $ctx->domain_axis;
+#             my $range = $ctx->range_axis;
+# 
+#             my $min = $range->range->lower;
+# 
+#             my $height = $self->height;
+# 
+#             my @vals = @{ $series->values };
+#             my @keys = @{ $series->keys };
+#             for(0..($series->key_count - 1)) {
+#                 my $x = $domain->mark($width, $keys[$_]);
+#                 my $y = $height - $range->mark($height, $vals[$_]);
+# 
+#                 $cr->move_to($x, $y);
+#                 $self->draw_point($cr, $x, $y, $series, $_);
+#             }
+#             my $color = $clicker->color_allocator->next;
+#             $cr->set_source_rgba($color->as_array_with_alpha);
+#             $cr->fill;
+#         }
+#     }
+# 
+#     return 1;
+# }
 
 sub draw_point {
     my ($self, $cr, $x, $y, $series, $count) = @_;
