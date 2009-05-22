c37942f7c3e5a7813a07315b04b18ee29492b2ba
Cory Watson <github@onemogin.com>
2009-05-22 17:15:14 -0500

    Better candlestick example.

--- example/candlestick.pl
+++ example/candlestick.pl
@@ -2,48 +2,56 @@
 use strict;
 
 use Chart::Clicker;
+use Chart::Clicker::Context;
 use Chart::Clicker::Data::DataSet;
+use Chart::Clicker::Data::Marker;
 use Chart::Clicker::Data::Series::HighLow;
 use Chart::Clicker::Renderer::CandleStick;
+use Geometry::Primitive::Rectangle;
 use Graphics::Color::RGB;
 
-my $cc = Chart::Clicker->new(width => 500, height => 250, format => 'png');
-
-my @hours = qw(
-
-);
+my $cc = Chart::Clicker->new(width => 500, height => 250);
 
 my $series1 = Chart::Clicker::Data::Series::HighLow->new(
     keys    => [qw(1 2 3 4 5 6 7 8 9 10)],
+
     highs   => [qw(5 9 7 8 8 9 5 4 7 9)],
-
-    lows    => [qw(1 4 2 3 1 4 1 1 1 4)],
     opens   => [qw(3 5 4 6 4 8 4 1 1 6)],
-    values  => [qw(5 4 6 4 8 4 1 1 6 9)]
+    values  => [qw(5 4 6 4 8 4 1 1 6 9)],
+    lows    => [qw(1 4 2 3 1 4 1 1 1 4)],
 );
 
-# We'll create a dataset with our first two series in it...
-my $ds = Chart::Clicker::Data::DataSet->new(
-    series => [ $series1 ]
+my $series2 = Chart::Clicker::Data::Series::HighLow->new(
+    keys    => [qw(1 2 3 4 5 6 7 8 9 10)],
+
+    highs   => [qw(9 7 9 7 9 8 6 6 8 9)],
+    opens   => [qw(3 6 4 6 4 8 4 1 1 6)],
+    values  => [qw(6 4 6 4 8 4 1 1 6 9)],
+    lows    => [qw(3 4 2 3 3 4 1 1 1 4)],
 );
 
-# Pretty stuff
-$cc->border->width(0);
+my $series3 = Chart::Clicker::Data::Series::HighLow->new(
+    keys    => [qw(1 2 3 4 5 6 7 8 9 10)],
 
-# Add the datasets to the chart
+    highs   => [qw(8 8 5 4 3 8 9 9 6 4)],
+    opens   => [qw(3 7 3 2 1 2 7 9 5 2)],
+    values  => [qw(7 3 2 1 2 7 9 5 2 3)],
+    lows    => [qw(1 2 2 0 1 4 1 1 1 4)],
+);
+
+my $ds = Chart::Clicker::Data::DataSet->new(series => [ $series1, $series2, $series3 ]);
+
 $cc->add_to_datasets($ds);
 
-# Set some labels on the default context
-my $defctx = $cc->get_context('default');
-$defctx->range_axis->label('Lorem');
-$defctx->range_axis->fudge_amount(.2);
-$defctx->domain_axis->label('Ipsum');
-$defctx->domain_axis->tick_label_angle(0.785398163);
-$defctx->domain_axis->fudge_amount(.06);
+my $def = $cc->get_context('default');
 
-# Here's the magic: You can set a renderer for any context.  In this case
-# we'll change the default to a Bar.  Voila!
-$defctx->renderer(Chart::Clicker::Renderer::CandleStick->new(bar_padding => 30));
+my $ren = Chart::Clicker::Renderer::CandleStick->new;
+$def->renderer($ren);
+$def->range_axis->tick_values([qw(1 4 7)]);
+$def->range_axis->format('%d');
+$def->domain_axis->fudge_amount(.05);
+$def->domain_axis->tick_values([qw(2 4 6 8 10)]);
+$def->domain_axis->format('%d');
 
 $cc->draw;
 $cc->write('foo.png');
