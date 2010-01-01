34b9ac3bd4172817b11597e34942d2fff94ebde1
Cory Watson <github@onemogin.com>
2010-01-01 10:34:14 -0600

    Version bump & manifest.

--- lib/Chart/Clicker.pm
+++ lib/Chart/Clicker.pm
@@ -24,7 +24,7 @@ use Chart::Clicker::Drawing::ColorAllocator;
 use Carp qw(croak);
 use Scalar::Util qw(refaddr);
 
-our $VERSION = '2.53';
+our $VERSION = '2.54';
 
 has '+background_color' => (
     default => sub {
